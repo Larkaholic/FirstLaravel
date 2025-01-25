997fadb2b26e574f3db3bec965202c09a1b8bac4
Larkaholic <orillosdominique@gmail.com>
2025-01-25 21:02:06 +0800

    deleted welcome.blade

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return 'Hello, Laravel!';
-});
 
 Route::get('/greet', [GreetController::class, 'showGreeting']);
